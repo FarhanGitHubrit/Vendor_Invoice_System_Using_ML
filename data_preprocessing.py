b37138059af7f5ae1f796d476376ec5b820e0b3b
Farhan Hussain <128406817+FarhanGitHubrit@users.noreply.github.com>
2026-03-06 17:10:57 +0530

    Add files via upload
    It is Inside Invoice flagging

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,32 +1,102 @@
+import pandas as pd
 import sqlite3
 from sklearn.model_selection import train_test_split
-import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
 
-def load_vendor_invoice_data(db_path: str):
+def load_invoice_data():
+    conn = sqlite3.connect("C:\\Users\\decent\\OneDrive\\Desktop\\Machine Learning Project\\inventory.db")
+
+    query = """
+    WITH purchase_agg AS(
+    SELECT
+        p.PONumber,
+        COUNT(DISTINCT p.Brand) as total_brands,
+        SUM(p.Quantity) as total_item_quantity,
+        SUM(p.Dollars) as total_item_dollars,
+        AVG(julianday(p.ReceivingDate) - julianday(p.PODate)) as avg_receiving_delay
+        FROM purchases p 
+        GROUP BY p.PONumber
+    )
+    SELECT
+        vi.PONumber,
+        vi.Quantity as invoice_quantity,
+        vi.Dollars as invoice_dollars,
+        vi.Freight,
+        (julianday(vi.InvoiceDate) - julianday(vi.PODate)) AS days_po_to_invoice,
+        (julianday(vi.PayDate) - julianday(vi.InvoiceDate)) AS days_to_pay,
+        pa.total_brands,
+        pa.total_item_quantity,
+        pa.total_item_dollars,
+        pa.avg_receiving_delay
+        
+    FROM vendor_invoice vi
+    LEFT JOIN purchase_agg pa
+        ON vi.PONumber = pa.PoNumber
+        
     """
-    Load vendor invoice data from SQlite database
-    """
-    conn = sqlite3.connect(db_path)
-    query = "SELECT * FROM vendor_invoice"
+
     df = pd.read_sql_query(query, conn)
     conn.close()
     return df
 
-def prepare_features(df: pd.DataFrame):
-    """
-    Select feature and target variable
-    """
-    X=df[["Dollars"]]
-    y=df[["Freight"]]
-    y = y.values.ravel() 
-    return X, y
+def create_invoice_risk_label(row):
+    if(abs(row["invoice_dollars"] - row["total_item_dollars"]) > 5):
+        return 1
+    if(row["avg_receiving_delay"] > 10):
+        return 1
+    return 0
 
-def split_data(X, y, test_size = 0.2, random_state = 42):
-    """
-    Split data into train and test sets
-    """
+def apply_labels(df):
+    df["flag_invoice"] = df.apply(create_invoice_risk_label, axis=1)
+    return df
+
+def split_data(df, features, target):
+    X = df[features]
+    y = df[target]
+
     return train_test_split(
-        X, y, test_size = test_size, random_state = random_state
+        X, y, test_size=0.2, random_state=42
     )
+
+def scale_features(X_train, X_test, scaler_path):
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    joblib.dump(scaler, 'models/scaler.pkl')
+    return X_train_scaled, X_test_scaled
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
